d3766df29f45619ea20fb9597b21cd3592fe5c5f
Monte McGuire <montem@hightouchinc.com>
2014-08-15 20:00:30 -0500

    Update specs.js

--- spec/specs.js
+++ spec/specs.js
@@ -1,10 +1,26 @@
 describe('quotes test', function() {
-	it('15 quotes returned', function() {
+	var quoteCount = 15;
+	var personCount = 3;
+
+	it('Last known count of quotes is ' + quoteCount, function() {
 		var a = buildQuotes();
-		a.length.should.equal(15);
+		a.length.should.equal(quoteCount);
 	});
-	it('3 people returned', function() {
+	it('Last known count of person is ' + personCount, function() {
 		var b = buildPerson();
-		b.length.should.equal(3);
+		b.length.should.equal(personCount);
+	});
+	it('Should set quote to Unseen after generating', function() {
+		var quoteArray = buildQuotes();
+		var seenCounter = 0;
+
+		generateRandomQuote(quoteArray);
+
+		for(var i=0;i<quoteArray.length;i++) {
+			var thisQuote = quoteArray[i].split("|");  // Quote, Who, Seen
+		  if (thisQuote[2] === "Seen")
+		  	seenCounter++;
+		}
+		seenCounter.should.equal(1);
 	});
 });
